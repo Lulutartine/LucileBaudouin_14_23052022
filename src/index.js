15ba37ba4816e4e9c78a36113cffbc560ed8c108
Lucile <lucile.baudouin@gmail.com>
2022-06-02 13:35:39 +0200

    App.js update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { theme } from './utils/style/theme';
+import { GlobalStyle } from './utils/style/GlobalStyle';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider as StyleComponentThemeProvider } from 'styled-components';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const ThemeProvider = ({ children, theme }) => {
+  return (
+    <StyleComponentThemeProvider theme={theme}>
+      <>
+        <GlobalStyle />
+        {children}
+      </>
+    </StyleComponentThemeProvider>
+  );
+};
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
