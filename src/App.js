178c59fc2bb64c4bed98357d53b58308878cb764
Lucile <lucile.baudouin@gmail.com>
2022-06-05 20:28:59 +0200

    App.js update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import { Routes, Route } from 'react-router-dom';
-import { Error } from './pages/Error';
-import { CreateEmployee } from './pages/CreateEmployee';
-import { EmployeeList } from './pages/EmployeeList';
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Footer from './components/Layout/Footer';
+import Header from './components/Layout/Header';
+import CreateEmployee from './pages/CreateEmployee';
+import EmployeeList from './pages/EmployeeList';
+
+const initialData = localStorage.getItem('employee');
 
 function App() {
+  const [currentList, setCurrentList] = useState(
+    initialData ? JSON.parse(initialData) : []
+  );
+
+  function addEmployee(newOne) {
+    currentList.push(newOne);
+    localStorage.setItem('employee', JSON.stringify(currentList));
+    setCurrentList(currentList);
+  }
+
   return (
-    <Routes>
-      <Route path="*" element={<Error />} />
-      <Route path="/" index element={<CreateEmployee />} />
-      <Route path="/employee-list" element={<EmployeeList />} />
-    </Routes>
+    <div className="bloc-page">
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={<CreateEmployee setCurrentList={addEmployee} />}
+        />
+        <Route
+          path="/employee-list"
+          element={<EmployeeList updateNewEmployee={currentList} />}
+        />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
