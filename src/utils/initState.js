46f5d40cd89ca9d03e952320d26101fe833dc7da
Lucile <lucile.baudouin@gmail.com>
2022-06-10 18:40:00 -0400

    add initState

---
+++ src/utils/initState.js
@@ -0,0 +1,11 @@
+export const initState = {
+  firstName: '',
+  lastName: '',
+  startDate: new Date(),
+  department: '',
+  dateOfBirth: new Date(),
+  street: '',
+  city: '',
+  state: '',
+  zipCode: '',
+};
