654f0d4d4ae6324b0520dd087d55a408590c68f8
Lucile <lucile.baudouin@gmail.com>
2022-06-10 15:52:33 -0400

    add helmet info

--- src/pages/CreateEmployee.jsx
+++ src/pages/CreateEmployee.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
+
 import Title from '../components/Title';
 import Form from '../components/Form';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const CreateEmployee = (props) => {
-  const setNewEmployee = props.setNewEmployee;
+  const addEmployee = props.setCurrentList;
 
   return (
-    <main className="container">
-      <section>
-        <Title title="Create Employee" />
-        <Form setNewEmployee={setNewEmployee} />
-      </section>
-    </main>
+    <HelmetProvider>
+      <Helmet>
+        <title>HRNet - Create Employee</title>
+      </Helmet>
+      <main className="container">
+        <section>
+          <Title title="Create Employee" />
+          <Form setCurrentList={addEmployee} />
+        </section>
+      </main>
+    </HelmetProvider>
   );
 };
 
 export default CreateEmployee;
+
+CreateEmployee.propTypes = {
+  setCurrentList: PropTypes.func.isRequired,
+};
