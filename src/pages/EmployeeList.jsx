a2140192135c355cf4e7a114bb2210c3a916eb08
Lucile <lucile.baudouin@gmail.com>
2022-06-17 10:09:22 -0400

    Clean code and comments

---
+++ src/pages/EmployeeList.jsx
@@ -0,0 +1,14 @@
+import { EmployeeTable } from '../components/EmployeeTable';
+import { Layout } from '../components/Layout';
+
+/**
+ * Render Employee List Page
+ * @returns {JSX}
+ */
+export const EmployeeList = () => {
+  return (
+<Layout title="Employee List">
+      <EmployeeTable />
+    </Layout>
+  );
+};
